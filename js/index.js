a8d07222dfc96aa48b2c5b23fb83b549b68d4178
protivaghosh <protivaghosh50@gmail.com>
2024-09-22 10:32:48 +0600

    added  make to calculate button result used

--- js/index.js
+++ js/index.js
@@ -8,4 +8,12 @@ calculateButton.addEventListener('click', function(){
 // console.log({income, software, courses, internet});
 const totalExpenses = software + courses + internet;
 const balance = income - totalExpenses;
+
+const totalExpensesElement = document.getElementById('total-expenses');
+totalExpensesElement.innerText = totalExpenses.toFixed(2);
+
+const balanceElement =document.getElementById('balance');
+balanceElement.innerText = balance.toFixed(2);
+const result = document.getElementById('results');
+result.classList.remove('hidden');
 })
