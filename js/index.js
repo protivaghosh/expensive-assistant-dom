a4d03a1a89c06b16abab42ad5f294b39885c27b9
protivaghosh <protivaghosh50@gmail.com>
2024-09-22 07:39:14 +0600

    make taka to income software cources and internet input added

--- js/index.js
+++ js/index.js
@@ -1,4 +1,11 @@
 const calculateButton =document.getElementById('calculate');
 calculateButton.addEventListener('click', function(){
-    console.log('thik aca')
+    
+    const income = parseFloat(document.getElementById('income').value);
+    const software = parseFloat(document.getElementById('software').value);
+    const courses = parseFloat(document.getElementById('courses').value);
+    const internet = parseFloat(document.getElementById('internet').value);
+// console.log({income, software, courses, internet});
+const totalExpenses = software + courses + internet;
+const balance = income - totalExpenses;
 })
