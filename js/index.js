c7683d32949d65429279b0a8d9815b33a8ffcc14
protivaghosh <protivaghosh50@gmail.com>
2024-09-22 17:37:15 +0600

    make added to savings amount button

--- js/index.js
+++ js/index.js
@@ -1,11 +1,12 @@
+// add event listener calculate button
 const calculateButton =document.getElementById('calculate');
 calculateButton.addEventListener('click', function(){
     
-    const income = parseFloat(document.getElementById('income').value);
-    const software = parseFloat(document.getElementById('software').value);
-    const courses = parseFloat(document.getElementById('courses').value);
-    const internet = parseFloat(document.getElementById('internet').value);
-// console.log({income, software, courses, internet});
+const income = parseFloat(document.getElementById('income').value);
+const software = parseFloat(document.getElementById('software').value);
+const courses = parseFloat(document.getElementById('courses').value);
+const internet = parseFloat(document.getElementById('internet').value);
+
 const totalExpenses = software + courses + internet;
 const balance = income - totalExpenses;
 
@@ -16,4 +17,28 @@ const balanceElement =document.getElementById('balance');
 balanceElement.innerText = balance.toFixed(2);
 const result = document.getElementById('results');
 result.classList.remove('hidden');
+})
+
+// add event listener saving button
+const calculateSavingsButton =document.getElementById('calculate-savings');
+calculateSavingsButton.addEventListener('click', function(){
+
+    const saving =parseFloat(document.getElementById('savings').value);
+    const income = parseFloat(document.getElementById('income').value);
+    const software = parseFloat(document.getElementById('software').value);
+    const courses = parseFloat(document.getElementById('courses').value);
+    const internet = parseFloat(document.getElementById('internet').value);
+   
+    const totalExpenses = software + courses + internet;
+    const balance = income - totalExpenses;
+
+    const savingAmount = (saving * balance) /100;
+    // console.log(savingAmount);
+    const remainingBalance = balance-savingAmount;
+
+    const savingElement =document.getElementById('savings-amount');
+    savingElement.innerText = savingAmount.toFixed(2);
+
+    const remainingElement =document.getElementById('remaining-balance');
+    remainingElement.innerText = remainingBalance.toFixed(2);
 })
