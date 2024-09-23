531c3b33e31f45620e3b635d719bd7e23cee6a93
protivaghosh <protivaghosh50@gmail.com>
2024-09-23 11:06:40 +0600

    added to history button and hidden to assistant button

--- js/index.js
+++ js/index.js
@@ -17,6 +17,19 @@ const balanceElement =document.getElementById('balance');
 balanceElement.innerText = balance.toFixed(2);
 const result = document.getElementById('results');
 result.classList.remove('hidden');
+
+// expensive button
+const historyItem  =document.createElement('div');
+historyItem.classList ='bg-white p-3 rounded-md border-1-2 border-indigo-500';
+
+historyItem.innerHTML =`
+ <p class="text-xs text-gray-500">${new Date().toLocaleDateString()}</p>
+ <p class="text-xs text-gray-500">income: $${income.toFixed(2)}</p>
+ <p class="text-xs text-gray-500">Expenses: $${totalExpenses.toFixed(2)}</p>
+ <p class="text-xs text-gray-500">Balance: $${balance.toFixed(2)}</p>
+`;
+const historyContainer =document.getElementById('history-list');
+historyContainer.insertBefore(historyItem, historyContainer.firstChild)
 })
 
 // add event listener saving button
@@ -41,4 +54,22 @@ calculateSavingsButton.addEventListener('click', function(){
 
     const remainingElement =document.getElementById('remaining-balance');
     remainingElement.innerText = remainingBalance.toFixed(2);
+})
+
+// history tab function
+const historyTab =document.getElementById('history-tab');
+const assistantTab =document.getElementById('assistant-tab');
+historyTab.addEventListener('click', function(){
+
+    historyTab.classList.add('text-white', 'bg-gradient-to-r', 'from-blue-500', 'to-purple-600');
+
+    historyTab.classList.remove('text-gray-600');
+
+    assistantTab.classList.remove('text-white', 'bg-gradient-to-r', 'from-blue-500', 'to-purple-600');
+    
+    assistantTab.classList.add('text-gray-600');
+
+    document.getElementById('expense-form').classList.add('hidden');
+    document.getElementById('history-section').classList.remove('hidden');
+
 })
