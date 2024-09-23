eea042050c37ecda19cf9820d73eafb0b701effd
protivaghosh <protivaghosh50@gmail.com>
2024-09-23 11:49:45 +0600

    added to assistant button tab and history button tab

--- js/index.js
+++ js/index.js
@@ -72,4 +72,15 @@ historyTab.addEventListener('click', function(){
     document.getElementById('expense-form').classList.add('hidden');
     document.getElementById('history-section').classList.remove('hidden');
 
+    // assistant button to history button
+    assistantTab.addEventListener('click', function(){
+
+        assistantTab.classList.add('text-white', 'bg-gradient-to-r', 'from-blue-500', 'to-purple-600');
+
+        historyTab.classList.remove('text-white', 'bg-gradient-to-r', 'from-blue-500', 'to-purple-600');
+ 
+        document.getElementById('expense-form').classList.remove('hidden');
+        document.getElementById('history-section').classList.add('hidden');
+    })
+
 })
